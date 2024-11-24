16682a6a2e3dd6d9e430097f325fbbea17f79857
imaun <imun22@gmail.com>
2024-11-24 22:53:34 +0100

    fetch data using remote url

--- src/Wdata.Lib/Sources/WebsiteRemoteSource.cs
+++ src/Wdata.Lib/Sources/WebsiteRemoteSource.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.Options;
 using Wdata.Contracts;
+using Wdata.Lib.Configuration;
 
 namespace Wdata.Sources;
 
+/// <summary>
+/// Fetch text data from a remote url.
+/// </summary>
 public class WebsiteRemoteSource : IWebsiteDataSource
 {
     private readonly string _baseUrl;
@@ -12,9 +17,31 @@ public class WebsiteRemoteSource : IWebsiteDataSource
         _baseUrl = baseUrl;
         _httpClient = httpClient;
     }
-    
-    public Task<string> FetchDataAsync(string path, CancellationToken cancel = default)
+
+    public WebsiteRemoteSource(IOptions<WdataConfig> config, HttpClient httpClient)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(config);
+        ArgumentNullException.ThrowIfNull(httpClient);
+
+        _httpClient = httpClient;
+
+        var remote_config = config.Value.GetRemoteSource();
+        if (remote_config is not null)
+        {
+            _baseUrl = remote_config.BasePath;
+        }
+    }
+    
+    public async Task<string> FetchDataAsync(string path, CancellationToken cancel = default)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+            throw new ArgumentNullException(nameof(path));
+        
+        var url = string.IsNullOrWhiteSpace(_baseUrl)
+            ? path.TrimStart('/')
+            : $"{_baseUrl.TrimEnd('/')}/{path.TrimStart('/')}";
+        
+        using var response = await _httpClient.GetAsync(url, cancel);
+        return await response.Content.ReadAsStringAsync(cancel);
     }
 }
