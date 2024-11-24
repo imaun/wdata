7663901905d39338b508da1bd96b9049614e57cb
imaun <imun22@gmail.com>
2024-11-24 21:09:58 +0100

    add WebsiteLocalSource

---
+++ src/Wdata.Lib/Sources/WebsiteLocalSource.cs
@@ -0,0 +1,25 @@
+using Wdata.Contracts;
+
+namespace Wdata.Lib.Sources;
+
+/// <summary>
+/// Fetch text data from file system.
+/// </summary>
+public class WebsiteLocalSource : IWebsiteDataSource
+{
+    private readonly string _basePath;
+
+    public WebsiteLocalSource(string basePath)
+    {
+        _basePath = basePath;
+    }
+    
+    public async Task<string> FetchDataAsync(string path, CancellationToken cancel = default)
+    {
+        var fullPath = Path.Combine(_basePath, path);
+        if (!File.Exists(fullPath))
+            throw new FileNotFoundException($"File not found: {fullPath}");
+        
+        return await File.ReadAllTextAsync(fullPath, cancel);
+    }
+}
